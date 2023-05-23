b223ae56ddc4a2cc35672365307b82422ce054d9
Rohit Jain <100214764+rohitjainfsl@users.noreply.github.com>
2023-05-23 13:49:34 +0530

    Added the edit code

--- src/todoRedux/features/todoSlice.js
+++ src/todoRedux/features/todoSlice.js
@@ -3,29 +3,36 @@ import { createSlice } from "@reduxjs/toolkit";
 export const todoSlice = createSlice({
   name: "todo",
   initialState: {
-    inputValue: "",
+    input: "",
     tasks: [],
     editing: false,
   },
   reducers: {
-    handleChange: (state, action) => {
-      state.inputValue = action.payload;
+    setInput: function (state, action) {
+      state.input = action.payload;
     },
-    handleFormSubmit: (state) => {
-      state.tasks = [...state.tasks, state.inputValue];
+    setTasks: function (state, action) {
+      if (state.editing === false) state.tasks = [...state.tasks, state.input];
+      else {
+        state.tasks = state.tasks.map((task, index) => {
+          return index === state.editing ? state.input : task;
+        });
+        state.editing = false;
+      }
+      state.input = "";
     },
-    handleTaskEdit: () => {},
-    handleTaskDelete: (state, action) => {
-        state.tasks = state.tasks.filter((task, key) => {
-          return key !== action.payload})
+    deleteTask: function (state, action) {
+      state.tasks = state.tasks.filter((task, index) => {
+        return index !== action.payload;
+      });
+    },
+    editTask: function (state, action) {
+      state.editing = action.payload;
+      state.input = state.tasks[action.payload];
     },
   },
 });
+export const { setInput, setTasks, deleteTask, editTask } =
+  todoSlice.actions;
 
-export const {
-  handleChange,
-  handleFormSubmit,
-  handleTaskEdit,
-  handleTaskDelete,
-} = todoSlice.actions;
 export default todoSlice.reducer;
