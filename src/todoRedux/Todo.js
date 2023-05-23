29617c3d79af74c062eab981a9e5f667e24cf158
Rohit Jain <100214764+rohitjainfsl@users.noreply.github.com>
2023-05-23 13:48:50 +0530

    Added the edit code

--- src/todoRedux/Todo.js
+++ src/todoRedux/Todo.js
@@ -1,61 +1,55 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { handleChange, handleFormSubmit, handleTaskDelete } from "./features/todoSlice";
+import { setInput, setTasks, deleteTask, editTask } from "./slices/todoSlice";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
-function Todo() {
+function Home() {
   const dispatch = useDispatch();
-
   const init = useSelector((state) => {
     return state.todo;
   });
 
-  //   function change(e){
-  //     dispatch(handleChange(e.target.value))
-  //   }
-
-  function submission(e) {
-    e.preventDefault();
-    dispatch(handleFormSubmit());
+  function handleSubmit(e){
+    e.preventDefault()
+    dispatch(setTasks())
   }
 
-  function handleDelete(e, index){
+  function handleDelete(e,index){
     e.preventDefault()
-    dispatch(handleTaskDelete(index))
+    dispatch(deleteTask(index))
+  }
+
+  function handleEdit(e,index){
+    e.preventDefault()
+    dispatch(editTask(index))
   }
 
   return (
-    <>
-      <h1>Todo</h1>
-      <form onSubmit={submission}>
-        {/* <input
-          type="text"
-          placeholder="Enter task"
-          value={init.inputValue}
-          onChange={change}
-        ></input> */}
-
+    <div id="todo">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Enter task"
-          value={init.inputValue}
-          onChange={(e) => dispatch(handleChange(e.target.value))}
-        ></input>
-
+          placeholder="Enter your task"
+          value={init.input}
+          onChange={(e) => dispatch(setInput(e.target.value))}
+        />
         <button type="submit">Add Task</button>
       </form>
       <ul>
-        {init.tasks.map((task, index) => {
-          return (
-            <li key={index}>
-              {task}
-              <a href="">Edit</a>
-              <a href="" onClick={(e) => handleDelete(e, index)}>Delete</a>
-            </li>
-          );
-        })}
+        {
+            init.tasks.map((task, index) => {
+                return <li key={index}>
+                    {task}
+                    <a href="" onClick={(e) => handleDelete(e, index)}><DeleteIcon /></a>
+                    <a href="" onClick={(e) => handleEdit(e, index)}><EditIcon /></a>
+                </li>
+            })
+        }
       </ul>
-    </>
+    </div>
   );
 }
 
-export default Todo;
+export default Home;
